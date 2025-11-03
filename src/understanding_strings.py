6325b27b01ea2b0983adf5fa4697f5432305af75
Javier2530401 <2530401@upv.edu.mx>
2025-11-02 23:15:48 -0600

    Modificasiones

---
+++ src/understanding_strings.py
@@ -0,0 +1,8 @@
+'''
+Un string es de manera sensilla una serie de caracteres .
+En py todo lo que se encuntra dentro de commilas simplis (') o comillas dobles (") es considerado un estring.
+'strings' y "string" son strings
+'''
+name = "clase de programacion"
+print (name)
+print (name. title())
---
+++ src/understanding_variables.py
@@ -0,0 +1,23 @@
+anoth_messaje = "I am really, really happy"
+mesaje = 
+print (anoth_messaje)
+"Print se ustilisa para mostrar mensajes"
+print (anoth_messaje, mesaje)
+'''
+Los valores  de variable en py deben deben nombrarse solo con
+- letras, numeros y giones bajos
+- deben comensar con un a letra o con un - pero no com numeros
+ejemplo
+- mensaje_1 (Y)
+- 1_mensaje (X)
+- no utilisar espacios para separar en los nombres de las variables
+- no utilisar palabras reserbadas de py para bariables o archibos
+- nombre cortos pero descriptibos
+- nombres en ingles
+- variables en minusculas
+'''
+'''
+traceback: Es un registro de donde el inteprete 
+           tubo problema al intentar ejecutarsu cdigo.
+
+'''
